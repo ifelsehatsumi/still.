0599e67a6c50743f405b1b3b83c673419af98daa
Hatsumi <61096970+ifelsehatsumi@users.noreply.github.com>
2021-11-09 17:42:05 -0500

    Update profile.py

--- Profile/profile.py
+++ Profile/profile.py
@@ -159,8 +159,8 @@ class Profile(commands.Cog):
         embed.set_thumbnail(url=user.avatar_url)
         embed.set_footer(text="\n".join(warnings))
         for option, value in profileDict.items():
-            if profileDict.items() == "":
-                warnings.append(f"Imagine not having a profile. <:ang_giggle:894993173686943754> Type `k,profileoptions` to see a list of stuff you can you add and set one up.")
+            if profileDict[option] == "":
+                await ctx.send(f"Imagine not having a profile. <:ang_giggle:894993173686943754> Type `k,profileoptions` to see a list of stuff you can you add and set one up.")
             return
             if profileDict['Location, TZ'] != "":
                 geo = profileDict['Location, TZ']
